ef60f3f0ae2f696501889e91c06c72303179d252
Karl Lehenbauer <karl@lehenbauer.com>
2023-05-16 19:17:00 -0500

    Add support for API key specifying and saving

--- tools/api/driver.py
+++ tools/api/driver.py
@@ -1,9 +1,8 @@
 
 
 import tkinter as tk
-from tkinter import filedialog
-
-import tkinter as tk
+from tkinter import filedialog, messagebox
+import os
 
 class MyGUI:
     def __init__(self, root):
@@ -11,6 +10,7 @@ class MyGUI:
         self.stem_vars = {}
         self.backing_track_vars = {}
         self.elements = ['vocals', 'drum', 'bass', 'piano', 'electric_guitar', 'acoustic_guitar', 'synthesizer', 'voice', 'strings', 'wind']
+        self.api_key = tk.StringVar()
         self.setup_gui()
 
 
@@ -42,16 +42,45 @@ class MyGUI:
         tk.Radiobutton(self.root, text="Phoenix", variable=self.splitter, value="phoenix").grid(row=len(self.elements)+3, column=1, sticky='w')
         tk.Radiobutton(self.root, text="Cassiopeia", variable=self.splitter, value="cassiopeia").grid(row=len(self.elements)+4, column=1, sticky='w')
 
-        tk.Button(self.root, text='Run', command=self.run_program).grid(row=len(self.elements)+6, column=0, columnspan=2)
+        # API Key
+        tk.Label(self.root, text="API Key", font=("Helvetica", 14, "bold")).grid(row=len(self.elements)+6, column=0, sticky='w')
+        tk.Entry(self.root, textvariable=self.api_key, width=16).grid(row=len(self.elements)+6, column=1, sticky='w')
+        tk.Button(self.root, text='Save', command=self.save_api_key).grid(row=len(self.elements)+6, column=2)
 
-        self.filter.set(1)
-        self.splitter.set("phoenix")
+        # Set default values
+        self.api_key.set(self.fetch_api_key())
+        self.filter.set(1)  # Default filter value
+        self.splitter.set("phoenix")  # Default splitter value
+
+        tk.Button(self.root, text='Run', command=self.run_program).grid(row=len(self.elements)+7, column=0, columnspan=2)
+
+    def save_api_key(self):
+        key = self.api_key.get().strip()
+        if len(key) != 16 or not all(c in '0123456789abcdefABCDEF' for c in key):
+            messagebox.showerror("Invalid API Key", "API Key must be 16 hexadecimal characters")
+            return
+        with open(".apikey", "w") as file:
+            file.write(key)
+
+    def fetch_api_key(self):
+        if os.path.exists(".apikey"):
+            with open(".apikey", "r") as file:
+                return file.read().strip()
+        return ""
 
     def run_program(self):
         stems = [stem for stem, var in self.stem_vars.items() if var.get()]
         backing_tracks = [track for track, var in self.backing_track_vars.items() if var.get()]
         filter = self.filter.get()
         splitter = self.splitter.get()
+        api_key = self.api_key.get().strip()
+        if api_key == '':
+            messagebox.showerror("No API Key", "Please set an API Key")
+            return
+
+        if len(stems) == 0 and len(backing_tracks) == 0:
+            messagebox.showerror("No Stems", "Please select at least one stem or backing track")
+            return
         file_path = filedialog.askopenfilename()
         print(f'"{file_path}"')
         print("Stems: ", stems)
